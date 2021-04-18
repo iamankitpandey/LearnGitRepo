ebfb420ac3d8159f4bb22ebe5c1d0527c7d5c39e
iamankitpandey <pandeykankit@gmail.com>
2021-04-18 22:44:41 +0530

    Now we will run while loop

--- gitforloop.java
+++ gitforloop.java
@@ -3,6 +3,8 @@ public class githello {
         for(int i=1;i<4;i++){
         System.out.println("Hello Git...");
         }
+        System.out.println("Sorry i dont to add loops");
+        System.out.println("I will add while loop.");
     }
     
 }
