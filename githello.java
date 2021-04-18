ba40d1d39884aff90674946224c60f7f68a77a72
iamankitpandey <pandeykankit@gmail.com>
2021-04-18 22:52:21 +0530

    Adding feature1

--- githello.java
+++ githello.java
@@ -1,6 +1,8 @@
 public class githello {
     public static void main (String[] args){
         System.out.println("Hello Git...");
+        System.out.println("Bhai you are selected in Google.");
     }
+    
     
 }
